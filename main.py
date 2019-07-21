290f9b165a5c9320a981eea8f70413a443f230d3
Lupe <guadapenaranda@gmail.com>
2019-07-21 18:11:55 -0300

    Se creo el main. Se generó la clase Clientes con dni aleatorios y categorias aletorias. Se generó los pasos temporales y que segun el mismo atienda gentes. Se abrió una sola caja nueva. Faltaria graficar y abrir mas cajas(?)

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random as r
+
+
 class Fila(object):
     """Clase base de fila"""
 
@@ -5,7 +8,7 @@ class Fila(object):
         """constructor de la clase Fila """
         self.enfila= 0
         self.fila = []#la fila es una lista
-
+##################
 class FilaPreferencial(Fila):
     """Clase de la fila de los clientes preferenciales"""        
 
@@ -16,15 +19,21 @@ class FilaPreferencial(Fila):
 
     def atender(self):
         """Atiende al proximo cliente preferencial"""
-        self.enfila-=1
-        self.fila.pop(0)
-    
-    def abrircajanueva(self,maxenfila,filanueva):
+        if (self.enfila > 0):#Si no entra nadie que no cuente
+            self.enfila-=1
+            self.fila.pop(0)
+        
+    def abrircajanueva(self,filanueva,maxenfila=20):
         """Si maxenfila es menor que la cantidad de clientes actualmente en espera, abro nueva caja"""
-        pass
+        if maxenfila < self.enfila:
+            #filanueva=FilaPreferencial()
+            self.enfila-=1
+
+            #filanueva.insertar(self.fila.pop(0))
+            filanueva.fila.append(self.fila.pop(0))#Inserto el cliente que saque a la filanueva
+            filanueva.enfila+=1   
     
-    
-    
+#######################   
 class FilaGeneral(Fila):
     """Clase que mantiene una fila de clientes no preferenciales"""
 
@@ -35,11 +44,11 @@ class FilaGeneral(Fila):
 
     def atender(self):
         """Atiende al proximo cliente general"""
-        self.enfila-=1
-        self.fila.pop(0)     
-
+        if (self.enfila > 0):#Si no entra nadie que no cuente
+            self.enfila-=1#al atender un cliente lo resta a la cantidad de clientes
+            self.fila.pop(0)#saca el objeto cliente de la fila(lista)   
     
-
+#######################
 class cliente(object):
     """clase cliente """
     def __init__(self,dni):
@@ -50,7 +59,81 @@ class cliente(object):
         """modifica el atributo categoria del cliente """
         self.categoria=categoria
   
+#############################MAIN#########################################
     
 if __name__ == "__main__":
     """ simular una fila en una entidad bancaria"""
-    pass
+    
+
+    #Filas que voy a necesitar 3
+
+    fp1=FilaPreferencial()
+    fp2=FilaPreferencial()
+    fg=FilaGeneral()
+
+
+    #Creo Clientes
+    #Se los adjudico a las filas segun su categoria
+    #Atender personas cada un rango de tiempo
+    cantidadpersonas=0
+
+    for t in range(1,300):
+
+        personanueva=r.randint(0,3)#gente que llega al banco
+        cantidadpersonas= cantidadpersonas + personanueva
+
+        for i in range(0,personanueva):
+
+            
+            dni=r.randint(1000,9000)
+            persona=cliente(dni)
+
+            #Quiero adjudicarle una categoria (general o preferencial)
+            n=r.randint(0,2)#Me elige aleatoriamente un numero entre 0 y 1
+            
+            if (n==0):
+                persona.modificarcategoria('General')
+                fg.insertar(persona)
+            else:
+                persona.modificarcategoria('Preferencial')
+                fp1.insertar(persona) 
+                fp1.abrircajanueva(fp2)        
+
+
+        #Le digo que atienda gente cada 2 o 3 pasos
+        if (t%2==0):
+        
+            fp1.atender()
+            fp2.atender()
+
+        if (t%3==0):
+
+            fg.atender()
+
+    print('total', 'General','preferencial1','preferencial2')         
+    print(cantidadpersonas,fg.enfila,fp1.enfila,fp2.enfila)
+        #print(fg.fila,fp1.fila)   
+
+
+        
+
+            
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
